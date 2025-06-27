cdcf9ca3c408ec83343c19f4dc6358a29709e90c
shisisan <shisisan5@gmail.com>
2025-06-27 14:31:08 +0700

    chore: remove unused assets and update asset references in code

--- src/shared/assets/assets.d.ts
+++ src/shared/assets/assets.d.ts
@@ -1,54 +1,20 @@
 declare const assets: {
-	badges: {
-		"example-badge.png": string
-	}
-	images: {
-		ui: {
-			"alert_dismiss.png": string
-			"backdrop.png": string
-			"blur.png": string
-			"button_glow_top.png": string
-			"circle.png": string
-			"heart.png": string
-			"heart_glow.png": string
-			"leader_pointer.png": string
-			"map_crosshair.png": string
-			"map_cursor.png": string
-			"nav_heart.png": string
-			"nav_heart_alt.png": string
-			"nav_home.png": string
-			"nav_home_alt.png": string
-			"nav_skins.png": string
-			"nav_skins_alt.png": string
-			"skin_card_gradient.png": string
-			"skin_indicator.png": string
-			"skin_indicator_locked.png": string
-			"tile_stripes.png": string
-			"vignette.png": string
-		}
-	}
-	marketing: {
-		"example-icon.png": string
-		"googly.png": string
-	}
-	sounds: {
-		"alert_bad.ogg": string
-		"alert_dismiss.ogg": string
-		"alert_money.ogg": string
-		"alert_neutral.ogg": string
-		"boost_start.ogg": string
-		"boost_stop.ogg": string
-		"button_down.ogg": string
-		"button_down_alt.ogg": string
-		"button_up.ogg": string
-		"button_up_alt.ogg": string
-		"error_1.ogg": string
-		"error_2.ogg": string
-		"error_3.ogg": string
-		"navigate.ogg": string
-		"start_game.ogg": string
-		"whoosh.ogg": string
-	}
-}
+    images: {
+        ui: {
+            'alert_dismiss.png': string;
+            'blur.png': string;
+        };
+    };
+    sounds: {
+        'alert_bad.ogg': string;
+        'alert_dismiss.ogg': string;
+        'alert_money.ogg': string;
+        'alert_neutral.ogg': string;
+        'button_down.ogg': string;
+        'button_down_alt.ogg': string;
+        'button_up.ogg': string;
+        'button_up_alt.ogg': string;
+    };
+};
 
-export = assets
+export = assets;
